c64f16fc1361e1e18e829d1222c8a845c48f7aed
TotallyIXL <157317899+TotallyIXL@users.noreply.github.com>
2024-02-10 09:49:23 -0500

    Create cloak.js

---
+++ cloak.js
@@ -0,0 +1,6 @@
+function cloak() {
+  var title = document.getElementById("cloakTabText").value;
+  var url = window.location;
+  var favicon = document.getElementById("cloakTabText-2").value;
+  cloakTab(title, url, favicon);
+}
