86250bb3b4efddf2f310ab341231d0e95ad2f9ce
TotallyIXL <157317899+TotallyIXL@users.noreply.github.com>
2024-02-25 15:23:10 -0500

    Create ct55555.js

---
+++ ct55555.js
@@ -0,0 +1,6 @@
+      function fullscreen() {
+        var title = "IXL | Math, Language Arts, Science, Social Studies, and Spanish"
+        var url = "https://example.com"
+        var favicon = "https://ixl.com/ixl-favicon.png"
+        cloakTab(title, url, favicon);
+      }
